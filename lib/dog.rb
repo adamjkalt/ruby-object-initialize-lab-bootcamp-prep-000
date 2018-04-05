b1389e091088d05feedc4a864d51a2b460701444
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-05 03:16:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,13 +13,13 @@ class Dog
      @name
    end
 
-  def initialize(breed)
+  #def initialize(breed)
+  #  @breed = breed
+  #end
+
+  def breed=(breed)
     @breed = breed
   end
-
-  #def breed=(breed)
-    #@breed = breed
-  #end
 
   def breed
     @breed
